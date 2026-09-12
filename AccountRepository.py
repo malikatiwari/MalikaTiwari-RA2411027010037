604f3fa4e8a888858861ee2d2ac8516a906a7428
malikatiwari <malikatiwari1407@gmail.com>
2026-09-12 16:27:26 +0530

    Section 4.3: define AccountRepository as storage abstraction

--- AccountRepository.py
+++ AccountRepository.py
@@ -1,4 +1,11 @@
-class AccountRepository:
+from abc import ABC, abstractmethod
 
+
+class AccountRepository(ABC):
+    @abstractmethod
     def save(self, account):
-        print("Saving account to database")
+        pass
+
+    @abstractmethod
+    def read_all(self):
+        pass
