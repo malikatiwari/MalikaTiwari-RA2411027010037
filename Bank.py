3ec79a6d2a9ba69180a171cb150ec1f0279f40d2
malikatiwari <malikatiwari1407@gmail.com>
2026-09-12 16:27:23 +0530

    Section 4.3: make Bank depend on AccountRepository interface

--- Bank.py
+++ Bank.py
@@ -1,6 +1,13 @@
+from AccountRepository import AccountRepository
+
+
 class Bank:
-    def __init__(self, notification_service):
+    def __init__(self, repository, notification_service):
+        self.repository: AccountRepository = repository
         self.notification_service = notification_service
+
+    def save_account(self, account):
+        self.repository.save(account)
 
     def notify(self, message):
         self.notification_service.send(message)
